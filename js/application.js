7c193510a685167d27d553635555a0104e502a16
MDoan75 <141285754+MDoan75@users.noreply.github.com>
2023-11-07 17:48:30 -0500

    Update application.js

---
+++ js/application.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+  $('body').on('input','.shopping input', function (){
+    var grandTotal = 0;
+    console.log('run')
+    var productCount = $(this).val();
+    var productPrice = parseFloat($(this).parent().prev().text().substring(1,3));
+    var subTotal = productPrice * productCount;
+    $(this).parent().next().next().html("$"+subTotal+".00");
+    $(".totalcost").each(function () {
+      if (/\d/.test($(this).text())) {
+        grandTotal += parseFloat($(this).text().substring(1));
+      }
+    
+    });
+
+    $('#totalprice').html("$"+grandTotal+".00");
+  });
+
+  $(document).on('click', '.btn.remove', function (){
+    $(this).parent().parent().remove();
+  });
+
+  $(document).on('click', '.btn.add', function (){
+    var product = $(this).parent().prev().prev().children("input").val();
+    var price = $(this).parent().prev().children("input").val();
+    $(this).parent().parent().before("<div class='row shopping'>" +
+    "<div class='col-xs-3 product'>"+product+"</div>" +
+    "<div class='col-xs-3 price'>$"+price+".00</div>" +
+    "<div class ='col-xs-3 pax'>QTY<input type='number' /></div>" +
+      "<div class ='col-xs-1 remove'><button class='btn btn-light btn-sm remove'>remove</button></div>" +
+      "<div class='col-xs-2 totalcost'>$--.--</div>" +
+    "</div>");
+ });
+});
